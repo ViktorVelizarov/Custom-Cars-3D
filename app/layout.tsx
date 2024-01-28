d40a43f221e0692131a093da081b49f084436150
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-01-28 12:37:34 +0200

    initialize project structure

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+
+ function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+
+  export default RootLayout;

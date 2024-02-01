922bbea8edb64bddcf715523952a4dc6b99833f3
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-02-01 14:06:13 +0200

    add a scene to render the cars in

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,11 @@
 
 import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PresentationControls } from "@react-three/drei";
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { useLoader } from '@react-three/fiber';
 import Model from 'components/Mustang';
+import Boxx from 'components/Boxx';
 
 export default function App() {
 
@@ -15,34 +16,14 @@ export default function App() {
   
   const galaxyTexture = useLoader(TextureLoader, "/galaxyMaterial.png");
   return (
-    <>
-    <div className='bg-blue-400 text-sm h-96 w-7/12'>
-      <Canvas>
-        <OrbitControls />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} castShadow/>
-        <Suspense fallback={null}>
-
-          <Model customColors={{body: bodyColor, accesoriesColor: accesoriesColor, rimsColor: rimsColor}}/>
-        </Suspense>
+    <div className="h-screen">
+    <Suspense fallback={null}>
+      <Canvas >
+        <color attach='background' args={["#213547"]}/>
+        <fog attach='fog' args={["#213547",10,30]}/>
+        <Boxx/>
       </Canvas>
+    </Suspense>
     </div>
-
-    <h2> Color Chooser</h2>
-    <input type="color" id="bodyColor" name="bodyColor"  value={bodyColor} 
-      onChange={(e) => setBodyColor(e.target.value)}>
-    </input>
-    <label for="bodyColor"> Body</label>
-
-    <input type="color" id="accesoriesColor" name="accesoriesColor"  value={accesoriesColor} 
-      onChange={(e) => setAccesoriesColor(e.target.value)}>
-    </input>
-    <label for="bodyColor"> Accesories</label>
-
-    <input type="color" id="rimsColor" name="rimsColor"  value={rimsColor} 
-      onChange={(e) => setRimsColor(e.target.value)}>
-    </input>
-    <label for="rimsColor"> Rims</label>
-    </>
   );
 }
